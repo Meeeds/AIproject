260399d737779ce10a6493edb54f407a4ddfe07d
Meeeds <louis.barjon@gmail.com>
2023-05-27 22:29:23 +0200

    with exploration and memory

--- chose_number.py
+++ chose_number.py
@@ -40,70 +40,94 @@ class MLP(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, 128),
+            nn.Linear(input_dim, 256),
             nn.ReLU(),
-            nn.Linear(128, 64),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(64, output_dim)
+            nn.Linear(128, output_dim)
         )
 
     def forward(self, x):
         return self.layers(x)
 
 class Player:
-    def __init__(self, learning_rate, discount_factor):
+    def __init__(self, learning_rate, memory_size, exploration_rate=0.1):
         self.nn = MLP(input_dim=2, output_dim=2)
         self.optimizer = optim.Adam(self.nn.parameters(), lr=learning_rate)
         self.loss_fn = nn.MSELoss()
-        self.discount_factor = discount_factor
 
-    def choose_action(self, game_state):
+        self.memory = []
+        self.memory_size = memory_size
+        self.exploration_rate = exploration_rate
+
+    def update_exploration_rate(self, win_rate):
+        self.exploration_rate = max(0.01, (1.0 - win_rate)**2)
+
+    def get_exploration_rate(self):
+        return self.exploration_rate
+
+    def choose_action(self, game_state, isTraing = False):
         state_tensor = torch.tensor([game_state['card_1'], game_state['card_2']], dtype=torch.float32)
         action_values = self.nn(state_tensor)
-        action_index = torch.argmax(action_values).item()
+        
+        # Add some exploration
+        if isTraing and np.random.rand() < self.exploration_rate:
+            action_index = np.random.randint(2)
+        else:
+            action_index = torch.argmax(action_values).item()
+
         return action_index
 
-    def learn(self, old_state, action_index, reward):
-        # Convert the state dictionaries into tensors
-        old_state_tensor = torch.tensor([old_state['card_1'], old_state['card_2']], dtype=torch.float32)
-        
-        reward = torch.tensor(reward, dtype=torch.float)  # convert reward to float
-        
+    def learn(self):
+        if len(self.memory) < self.memory_size:
+            return
+
+        #print("learn")
+        # Sample a batch of experiences from memory
+        states, actions, rewards = zip(*self.memory)
+
+        # Convert to tensors
+        states = torch.tensor(states, dtype=torch.float32)
+        actions = torch.tensor(actions, dtype=torch.int64)
+        rewards = torch.tensor(rewards, dtype=torch.float32)
+
         self.optimizer.zero_grad()
 
         # Compute Q(s,a)
-        q_values_old = self.nn(old_state_tensor)
-        q_value_old = q_values_old[action_index].unsqueeze(0) # Add an extra dimension to the predicted Q-value
+        q_values = self.nn(states)
+        q_values = q_values.gather(1, actions.unsqueeze(1)).squeeze()
 
         # Compute the target value
-        target = reward
+        targets = rewards
 
         # Compute the loss
-        loss = self.loss_fn(q_value_old, torch.tensor([target]))
+        loss = self.loss_fn(q_values, targets)
 
         # Backpropagate the loss
         loss.backward()
 
         # Update the weights
         self.optimizer.step()
-        #print(f'Loss: {loss.item()}', end='')
-        #for name, param in self.nn.named_parameters():
-            #if param.requires_grad:
-                #print(name, param.data)
+        #erase memory
+        self.memory = []
+
+    def remember(self, state, action, reward):
+        self.memory.append((state, action, reward))
+        if len(self.memory) > self.memory_size:
+            print("errasing a training")
+            sys.exit()
 
 
 def train(player, game, num_games):
     player_actions = defaultdict(int)
     for i in range(num_games):
-        #print(f"training game {i}")
         game.reset()
-        action_index = player.choose_action(game.get_state())
+        action_index = player.choose_action(game.get_state(), True)
         reward = game.chose_card(action_index)
-        #print(f"player {reward} {action_index}")
         player_actions[action_index]+=1
-        player.learn(game.get_state(), action_index, reward)
-
-    #print('player_actions ' , dict(player_actions))
+        state = game.get_state() # get the state after the action
+        player.remember((state['card_1'], state['card_2']), action_index, reward) # remember the state, action and reward
+        player.learn()
     
 
 def test(player, game, num_games):
@@ -128,22 +152,17 @@ def test(player, game, num_games):
         else:
             player_chose_card_2+=1
 
-    #print('Player wins:', player_wins)
-    #print('chose first card', player_chose_card_1)
-    #print('chose second card', player_chose_card_2)
-    #print('game_lost', game_lost)
-
     return player_wins
 
 # In your main function:
 def main():
     # Instantiate the game
-    int_max_to_chose = 100
+    int_max_to_chose = 1000
     game = CardGame(int_max_to_chose)
     print(f"learning to chose between number in [1,{int_max_to_chose}]")
     win_percent = 0
     # Instantiate the players
-    player = Player(learning_rate=0.01, discount_factor=0.99)
+    player = Player(learning_rate=0.01, memory_size = 100)
     increment_training_games = 1000
     total_training = 0
     test_games = 1000
@@ -154,7 +173,9 @@ def main():
         total_training+=increment_training_games
         wins = test(player, game, test_games)
         win_percent = 100.0 * wins / (1.0*test_games)
-        print(f"after {total_training} trainig we got {win_percent}% wins")
+        # Update exploration rate based on win percent
+        player.update_exploration_rate(win_percent/100) 
+        print(f"after {total_training} trainig we got {win_percent}% wins, exploration rate {player.get_exploration_rate()}")
 
 
 if __name__ == "__main__":
